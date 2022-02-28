0473ecce832d280a803dfa8f56357c915b6f8abe
aurelien fache <aurelien.fache@gmail.com>
2022-02-28 15:40:23 +0100

    add client python

---
+++ get_order_by_pusher.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.9
+
+import pysher
+import sys
+import time
+
+# Add a logging handler so we can see the raw communication data
+import logging
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+ch = logging.StreamHandler(sys.stdout)
+root.addHandler(ch)
+
+pusher = pysher.Pusher('73bcb26ed6d04c63889a',cluster='eu')
+
+def write_status_sextoy(write_vitesse):
+	f = open("sextoy.txt", "w")
+	f.write(write_vitesse)
+	f.close()
+
+
+def  my_func(*args, **kwargs):
+    print("processing Args:", args)
+    write_status_sextoy(args[0])
+    print("processing Kwargs:", kwargs)
+
+# We can't subscribe until we've connected, so we use a callback handler
+# to subscribe when able
+def connect_handler(data):
+    channel = pusher.subscribe('my-channel')
+    channel.bind('my-event', my_func)
+
+pusher.connection.bind('pusher:connection_established', connect_handler)
+pusher.connect()
+
+while True:
+    # Do other things in the meantime here...
+    time.sleep(1)
